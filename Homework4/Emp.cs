af9baf86455b6d26de8f6db04afa84f20bc57069
kachiZ <lihourouk@gmail.com>
2023-12-07 22:06:06 +0700

    init

--- Homework4/Emp.cs
+++ Homework4/Emp.cs
@@ -2,23 +2,88 @@
 {
     internal class Emp
     {
-        private int EmpId;
-        private string EmpName;
-        private string EmpGender;
-        private string EmpBOD;
-        private string EmpAddress;
-        private string EmpTel;
-        public Emp() { };
+        private int empId;
+        private string empName;
+        private string empGender;
+        private string empBOD;
+        private string empAddress;
+        private string empTel;
+        public Emp() { }
 
         public Emp(int id, string name, string gender, string bod, string address, string tel)
         {
-            EmpId = id;
-            EmpName = name;
-            EmpGender = gender;
-            EmpBOD = bod;
-            EmpAddress = address;
-            EmpTel = tel;
+            empId = id;
+            empName = name;
+            empGender = gender;
+            empBOD = bod;
+            empAddress = address;
+            empTel = tel;
         }
+
+        public int EmpId
+        {
+            get { return empId; }
+            set { empId = value; }
+        }
+
+        public string EmpName
+        {
+            get { return empName; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+                empName = value;
+            }
+        }
+
+        public string EmpGender
+        {
+            get { return empGender; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+                empGender = value;
+            }
+        }
+
+        public string EmpBOD
+        {
+            get { return empBOD; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+                empBOD = value;
+            }
+        }
+
+        public string EmpAddress
+        {
+            get { return empAddress; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+                empAddress = value;
+            }
+        }
+
+        public string EmpTel
+        {
+            get { return empTel; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("value");
+                empTel = value;
+            }
+        }
+
+
+
+
         public void ShowInfo()
         {
             Console.WriteLine($"Employee ID: {EmpId}");
